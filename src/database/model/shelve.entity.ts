e8fdcf2410dc4a073e653603d2114f108dc9a72c
Admin Stagiaire <justinecoulon1@gmail.com>
2025-03-31 15:52:10 +0200

    shelve entity

---
+++ src/database/model/shelve.entity.ts
@@ -0,0 +1,41 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { JoinColumn } from 'typeorm/browser';
+import { User } from './user.entity';
+import { Length } from 'class-validator';
+
+enum ShelveType {
+  'READ' = 'READ',
+  'TO_READ' = 'TO_READ',
+  'READING' = 'READING',
+}
+
+@Entity()
+export class Shelve {
+  @PrimaryGeneratedColumn({ name: 'shelve_id' })
+  id: number;
+
+  @Column({ length: 200 })
+  @Length(1, 200)
+  name: string;
+
+  @Column()
+  type: ShelveType;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.shelves)
+  @JoinColumn({ name: 'app_user_id' })
+  user: Promise<User>;
+
+  constructor(name: string, type: ShelveType, createdAt: Date, updatedAt: Date, user: User) {
+    this.name = name;
+    this.type = type;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.user = Promise.resolve(user);
+  }
+}
