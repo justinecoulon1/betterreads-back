f12f21308a5b92a0145bd7f2fa04eb39318d53be
Admin Stagiaire <justinecoulon1@gmail.com>
2025-03-31 15:52:01 +0200

    review entity

---
+++ src/database/model/review.ts
@@ -0,0 +1,33 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { JoinColumn } from 'typeorm/browser';
+import { User } from './user.entity';
+
+@Entity()
+export class Review {
+  @PrimaryGeneratedColumn({ name: 'review_id' })
+  id: number;
+
+  @Column()
+  commentary: string;
+
+  @Column()
+  score: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.reviews)
+  @JoinColumn({ name: 'app_user_id' })
+  user: Promise<User>;
+
+  constructor(commentary: string, score: number, createdAt: Date, updatedAt: Date, user: User) {
+    this.commentary = commentary;
+    this.score = score;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.user = Promise.resolve(user);
+  }
+}
